2e6fb9ebaf52ceeb25289dd6c91c9036df4582ed
natascha030203 <natascha030203@gmail.com>
2024-12-12 11:03:05 +0100

    mad, kaffe, og forum

--- script.js
+++ script.js
@@ -20,6 +20,27 @@ function indlosBilletten() {
 
 
 
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+function myFunction(zoneIndex) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+        if (i === zoneIndex) {
+            dropdowns[i].classList.toggle("show");
+        } else {
+            dropdowns[i].classList.remove("show");
+        }
+    }
+}
 
-
-
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
